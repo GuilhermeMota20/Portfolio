9fbc4d027e2897b528e88553aeff24357f0383ef
GuilhermeMota20 <guidroidbr.2007@gmail.com>
2021-07-26 00:29:31 -0300

    :tada: final commit

---
+++ assets/js/swiper.js
@@ -0,0 +1,15 @@
+// TESTIMONIALS CAROUSEL SLIDER SWIPERJS
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1.5,
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true,
+    breakpoints : {
+        767: {
+            slidesPerView: 2.5,
+            setWrapperSize: true
+        }
+    }
+});
